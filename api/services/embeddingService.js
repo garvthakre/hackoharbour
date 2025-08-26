7a64ae32a322ad26c1912825424ab1568858eb9d
Adnan <technicaljigsaw.tj@gmail.com>
2025-08-26 23:13:46 +0530

    fix: ui, client side redirection

--- api/services/embeddingService.js
+++ api/services/embeddingService.js
@@ -6,7 +6,6 @@ dotenv.config();
 
 // Initialize HuggingFace embeddings model
 const createEmbeddingsService = () => {
-  console.log("HF key: ", process.env.HF_API_KEY)
   if(!process.env.HF_API_KEY) {
     throw new Error("Hugging Face API key is required");
   }
